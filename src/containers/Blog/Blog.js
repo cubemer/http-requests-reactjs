6e3661f3dea2e41b428a4561ec9fda7499ea2106
Juan Carmona <xjuanchovillax@gmail.com>
2019-05-15 19:27:19 -0600

    Add ability to select individual post
    I added the functionality to select and display a single post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,29 +8,45 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        this.setState({posts: response.data})
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Juan'
+          }
+        });
+        this.setState({posts: updatedPosts})
       });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id})
+  };
+
   render() {
     const posts = this.state.posts.map(post => (
-      <Post key={post.id} title={post.title} />
+      <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)}/>
     ));
 
 
     return (
       <div>
-        <section className="Posts">
+        <section className='Posts'>
           {posts}
         </section>
         <section>
-          <FullPost/>
+          <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost/>
